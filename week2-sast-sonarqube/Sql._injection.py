ef4034629cae706ab42b761f99a13c7c7d096e13
lineomakhakhe4-wq <lineomakhakhe4@gmail.com>
2026-02-17 21:25:26 +0530

    add:Sql_injection

--- week2-sast-sonarqube/Sql._injection.py
+++ week2-sast-sonarqube/Sql._injection.py
@@ -1,19 +1,9 @@
 import sqlite3
-import subprocess
-
-def dangerous(user_input):
-    subprocess.call(user_input, shell=True)
-
 
 def get_user(username):
     conn = sqlite3.connect("users.db")
     cursor = conn.cursor()
 
-    # Intentional SQL Injection vulnerability
     query = f"SELECT * FROM users WHERE username = '{username}'"
-
     cursor.execute(query)
     return cursor.fetchall()
-
-eval("print('hello')")
-
